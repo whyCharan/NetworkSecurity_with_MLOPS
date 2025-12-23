f954303f45e0ee3bc2a1a28ac94d8f86021f723a
Vinayaka Charan <charan.sandaka5@gmail.com>
2025-12-23 13:06:19 +0530

    Checking whether mongodb lineked or not linked

---
+++ test_mongodb.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+
+load_dotenv()
+uri = os.getenv('MONGO_DB_URL')
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
